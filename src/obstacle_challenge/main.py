f0141691ee121e29308da116c2da0a25dc34542d
wro-fe-2026 <wro2026fe@gmail.com>
2026-04-24 12:12:50 -0600

    Create main.py
    Add src/obstacle_challenge preliminary code structure

---
+++ src/obstacle_challenge/main.py
@@ -0,0 +1,9 @@
+# WRO 2026 Future Engineers - CONALEP Chilpancingo
+# Obstacle Challenge - Preliminary code
+# Team: Josue Cervantes, Uriel Silva, Eduardo Martinez
+
+# TODO: Add color detection for red/green pillars
+# TODO: Add obstacle avoidance logic
+# TODO: Add parking maneuver code
+
+print("WRO 2026 - Obstacle Challenge - starting...")
