7ac6dd9555f7be58961faf2a3dd0c04df5c15cd5
wro-fe-2026 <wro2026fe@gmail.com>
2026-04-24 12:11:02 -0600

    Create main.py
    Add src/open_challenge preliminary code structure

---
+++ src/open_challenge/main.py
@@ -0,0 +1,9 @@
+# WRO 2026 Future Engineers - CONALEP Chilpancingo
+# Open Challenge - Preliminary code
+# Team: Josue Cervantes, Uriel Silva, Eduardo Martinez
+
+# TODO: Add motor control code
+# TODO: Add servo control code
+# TODO: Add sensor reading code
+
+print("WRO 2026 - Open Challenge - starting...")
